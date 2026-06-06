8a16bd333339171e8377b0b597b9eee727163433
Hatice <ercanhaticenur07@gmail.com>
2026-06-06 21:17:01 +0300

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -1,3 +1,3 @@
 // Google AI Studio'dan aldığınız ücretsiz API Anahtarını buraya girin.
 // Link: https://aistudio.google.com/
-export const GEMINI_API_KEY = 'AQ.Ab8RN6Ixn77uEI931tdXLfE2UEVwpDSpE5OW0XW-oQqaAqq5ig';
+export const GEMINI_API_KEY = 'BURAYA_GEMINI_API_ANAHTARINIZI_YAZIN';
